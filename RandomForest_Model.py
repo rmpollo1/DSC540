80192c4f0140d7cf278c6be0e27c8df921a434ae
Ross <rmpollo1@gmail.com>
2020-02-15 16:12:56 -0800

    Added Random State

--- RandomForest_Model.py
+++ RandomForest_Model.py
@@ -1,10 +1,18 @@
 import numpy as np 
+import pandas as pd 
 import csv
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold
 from sklearn.metrics import accuracy_score, roc_auc_score
 from imblearn.under_sampling import RandomUnderSampler
+
+##########################################
+# Global Parameters
+rand_st = 132
+#
+#
+##########################################
 
 ##########################################
 # Data Import
@@ -52,18 +60,25 @@ target_np=np.asarray(target)
 #
 #
 ##########################################
-data_train, data_validate, target_train, target_validate = train_test_split(data_np,target_np,train_size=10_000)
+data_train, data_validate, target_train, target_validate = train_test_split(data_np,target_np,train_size=1_000,random_state=rand_st)
 ##########################################
 # Grid Search
 params = {"n_estimators":[5,10,20,50,100]}
 scorers = {"Accuracy":'accuracy',"AUC":'roc_auc'}
-clf = RandomForestClassifier()
+clf = RandomForestClassifier(random_state=rand_st)
 
-rus = RandomUnderSampler()
+rus = RandomUnderSampler(random_state=rand_st)
 data_res, target_res = rus.fit_resample(data_train, target_train)
 
-grid = GridSearchCV(clf,param_grid=params,scoring=scorers,cv=5,refit='Accuracy')
-grid.fit(data_res,target_res)
-print(grid.cv_results_)
+cross_val = StratifiedKFold(n_splits=5,random_state=rand_st)
 
-print(grid.score(data_validate,target_validate))
+grid = GridSearchCV(clf,param_grid=params,scoring=scorers,cv=cross_val,refit='Accuracy')
+grid.fit(data_res,target_res)
+rf_results = pd.DataFrame(grid.cv_results_)
+rf_results.to_csv("./RandomForestResults.csv",index=False)
+
+for _,row in rf_results.iterrows():
+    print("Random Forest Metrics (Number of Trees {})".format(row['param_n_estimators']))
+    print("Accuracy: {:0.2f} ( +/- {:0.2f})".format(row['mean_test_Accuracy'],2*row['std_test_Accuracy']))
+    print("AUC: {:0.2f} ( +/- {:0.2f})".format(row['mean_test_AUC'],2*row['std_test_AUC']))
+    print("Runtime: {:0.4f}".format(row['mean_fit_time']))

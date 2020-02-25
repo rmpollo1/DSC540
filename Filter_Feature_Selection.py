bb08f036a65398504937857d97381854dd85c8e3
Ross <rmpollo1@gmail.com>
2020-02-25 07:39:22 -0800

    Add Models

---
+++ Filter_Feature_Selection.py
@@ -0,0 +1,101 @@
+# Data Util Libs
+import pandas as pd 
+import numpy as np 
+# ML Libs
+from sklearn.feature_selection import chi2, f_classif, mutual_info_classif, SelectKBest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+from imblearn.under_sampling import RandomUnderSampler
+
+
+##########################################
+# Globals 
+
+# Scoring Metrics
+scorers = {
+        "Accuracy":'accuracy',"AUC":'roc_auc',
+        'F1':'f1','bAcc':'balanced_accuracy',
+        'precision':'precision'
+    }
+
+# Results Collector
+full_results = pd.DataFrame()
+##########################################
+
+# Read In Training Data
+df = pd.read_csv("./Data/Training_Data.csv")
+
+# Seperate Target from Feature
+# Convert to Numpy Array
+target = np.array(df['approve'])
+df = df.drop('approve',axis=1)
+feature_names = df.columns 
+df = np.array(df)
+
+# Feature Scaling
+scaler = MinMaxScaler()
+df = scaler.fit_transform(df)
+
+# Filter Based Feature Selectors 
+chi2_flt = SelectKBest(score_func=chi2)
+chi2_flt.fit(df,target)
+f_flt = SelectKBest(score_func=f_classif)
+f_flt.fit(df,target)
+mi_flt = SelectKBest(score_func=mutual_info_classif)
+mi_flt.fit(df,target)
+
+filters = [chi2_flt,f_flt,mi_flt]
+filter_names = ["Chi2","F_Classif","Mutual Info"]
+
+# Resampling 
+df, target = RandomUnderSampler().fit_resample(df,target)
+
+# Base Classifiers
+clf_names = ["Random Forest","Linear SVM","MLP"]
+rf_clf = RandomForestClassifier(n_estimators=20,criterion='gini',min_samples_split=3)
+svm_clf = LinearSVC(C=1.0)
+mlp_clf = MLPClassifier(hidden_layer_sizes=(8,),activation='relu',solver='adam')
+clfs = [rf_clf,svm_clf,mlp_clf]
+
+# Cross Validation Stategy 
+cv = StratifiedKFold(n_splits=5)
+
+# Number of Features to Select
+for k in [5,10,15,20,30,40,len(feature_names)]:
+    # Feature Selection Methods 
+    # Chi2, F_Classif, & Mutual Info
+    for flt_name, flt in zip(filter_names,filters):
+        # Set Number of Features to Select
+        flt.set_params(**{'k':k})
+        # Transform Dataset
+        df_sub = flt.transform(df)
+    
+        # Cross Validate Classifiers
+        # Random Forest ( n_trees=20 )
+        # Linear SVM ( C=1.0 )
+        # MLP ( Hidden Layer = (8,) )
+        for clf_name,clf in zip(clf_names,clfs):
+            grid = GridSearchCV(clf,param_grid={},scoring=scorers,refit='precision',cv=cv)
+            grid.fit(df_sub,target)
+
+            cv_res = pd.DataFrame(grid.cv_results_)
+            cv_res['Classifier'] = clf_name
+            cv_res['Filter Method'] = flt_name 
+            cv_res['Num Features'] = k
+            cv_res['Features Selected'] = '|'.join([f for f,u in zip(feature_names,flt.get_support()) if u])
+            full_results = full_results.append(cv_res,ignore_index=True)
+            for _,row in cv_res.iterrows():
+                print("Number of Features: {}".format(k))
+                print("{} Metrics".format(clf_name))
+                print("\tAccuracy: {:0.3f} (+/- {:0.3f})".format(row['mean_test_Accuracy'],2*row['std_test_Accuracy']))
+                print("\tBalanced Acc: {:0.3f} (+/- {:0.3f})".format(row['mean_test_bAcc'],2*row['std_test_bAcc']))
+                print("\tAUC: {:0.3f} (+/- {:0.3f})".format(row['mean_test_AUC'],2*row['std_test_AUC']))
+                print("\tF1: {:0.3f} (+/- {:0.3f})".format(row['mean_test_F1'],2*row['std_test_F1']))
+                print("\tPrecision: {:0.3f} (+/- {:0.3f})".format(row['mean_test_precision'],2*row['std_test_precision']))
+                print("\tMean Fit Time: {:0.4f}".format(row['mean_fit_time']))
+
+full_results.to_csv("./Results/Feature_Selection_Results_3.csv",index=False)
---
+++ MLP_Model.py
@@ -0,0 +1,165 @@
+import sys
+import numpy as np 
+import pandas as pd 
+import csv
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold
+from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, balanced_accuracy_score, precision_score
+from sklearn.metrics import confusion_matrix
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, SelectFromModel
+from sklearn.feature_selection import chi2, f_classif, mutual_info_classif
+
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
+
+##########################################
+# Global Parameters
+resampling = 1                  # Resampling Switch
+resampling_method = 1           # Undersampling = 1; Oversampling = 2
+fs = 0                          # Feature Selection Switch
+fs_method = 3                   # Feature Selection Method
+fs_scorer = chi2                # Filter Selection Scorer
+#
+#
+##########################################
+
+##########################################
+# Cache Results
+try:
+    res_file = sys.argv[1]
+except IndexError as e:
+    res_file = "RandomForest_Results.csv"
+    print("No Result File Supplied saving results in RandomForest_Results.csv")
+
+#
+#
+##########################################
+
+##########################################
+# Data Import
+#
+target_idx = 0 
+feat_start = 1
+
+f = csv.reader(open("./Data/Training_Data.csv"), delimiter = ',', quotechar = '"')
+
+header = next(f)
+
+#Read data
+data=[]
+target=[]
+for i,row in enumerate(f):
+    # For Testing Scipt read first 10k samples
+    #if i == 150_000:
+    #    break 
+
+    #Load Target
+    if row[target_idx]=='':                         #If target is blank, skip row                       
+        continue
+    else:
+        target.append(float(row[target_idx]))       #If pre-binned class, change float to int
+
+    #Load row into temp array, cast columns  
+    temp=[]
+                 
+    for j in range(feat_start,len(header)):
+        if row[j]=='':
+            temp.append(float())
+        else:
+            temp.append(float(row[j]))
+
+    #Load temp into Data array
+    data.append(temp)
+  
+#Test Print
+print(header)
+print(len(target),len(data))
+print('\n')
+
+data_np=np.asarray(data)
+target_np=np.asarray(target)
+#
+#
+##########################################
+
+##########################################
+# Base Estimator (Random Forest)
+clf = MLPClassifier()
+
+##########################################
+
+##########################################
+# Feature Selection
+if fs == 1: 
+    # Apply Feature Selection 
+
+    if fs_method == 1:
+        print("--- LV Filter ON ---")
+        flt = VarianceThreshold(threshold=0.5)
+        data_np = flt.fit_transform(data_np,target_np)
+    
+    if fs_method == 2:
+        print("--- Feature Selection ON ---")
+        flt = SelectKBest(score_func=fs_scorer,k=20)
+        data_np = flt.fit_transform(data_np,target_np)
+    
+    if fs_method == 3:
+        print("--- Embedded Feature Selection ON ---")
+        flt = SelectFromModel(clf, prefit=False, threshold='mean', max_features=None)
+        data_np = flt.fit_transform(data_np,target_np)
+
+    selected = []
+    removed = []
+    for i,c in zip(flt.get_support(),header[1:]):
+        if i:
+            selected.append(c)
+        else:
+            removed.append(c)
+    print("Features Selected: ",selected)
+    print("Features Removed: ",removed)
+    print("Selected/Removed/Total: (",len(selected),len(removed),len(header)-1,")")
+#
+##########################################
+
+##########################################
+# Resampling 
+if resampling == 1:
+    print("--- Resampling ON ---")
+    if resampling_method == 1:
+        print("--- Undersampling ON ---")
+        rs = RandomUnderSampler()
+    if resampling_method == 2:
+        print("--- Oversampling ON ---")
+        rs = RandomOverSampler()
+    data_np, target_np = rs.fit_resample(data_np,target_np)
+    print("Resampled Training Data Samples: {}".format(len(target_np)))
+
+##########################################
+
+##########################################
+# Grid Search
+params = {"hidden_layer_sizes":[(8,)]}
+scorers = {
+    "Accuracy":'accuracy',"AUC":'roc_auc',
+    'F1':'f1','bAcc':'balanced_accuracy',
+    'precision':'precision'
+}
+cross_val = StratifiedKFold(n_splits=5)
+
+grid = GridSearchCV(clf,param_grid=params,scoring=scorers,cv=cross_val,refit='precision')
+grid.fit(data_np,target_np)
+rf_results = pd.DataFrame(grid.cv_results_)
+rf_results.to_csv(res_file,index=False)
+
+for _,row in rf_results.iterrows():
+    print("Multi Layer Perceptron Metrics (Hidden Layers {})".format(row['param_hidden_layer_sizes']))
+    print("\tAccuracy: {:0.3f} (+/- {:0.3f})".format(row['mean_test_Accuracy'],2*row['std_test_Accuracy']))
+    print("\tBalanced Acc: {:0.3f} (+/- {:0.3f})".format(row['mean_test_bAcc'],2*row['std_test_bAcc']))
+    print("\tAUC: {:0.3f} (+/- {:0.3f})".format(row['mean_test_AUC'],2*row['std_test_AUC']))
+    print("\tF1: {:0.3f} (+/- {:0.3f})".format(row['mean_test_F1'],2*row['std_test_F1']))
+    print("\tPrecision: {:0.3f} (+/- {:0.3f})".format(row['mean_test_precision'],2*row['std_test_precision']))
+    print("\tMean Fit Time: {:0.4f}".format(row['mean_fit_time']))
+##########################################
+
+

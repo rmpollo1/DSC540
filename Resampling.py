45855c052133582543697adf0800cef5f705610d
Ross <rmpollo1@gmail.com>
2020-03-03 16:37:32 -0800

    removed balanced RF

--- Resampling.py
+++ Resampling.py
@@ -5,7 +5,6 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
-from imblearn.ensemble import BalancedRandomForestClassifier
 # Utilities
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
@@ -62,7 +61,6 @@ under_rs = RandomUnderSampler()
 rf_clf = RandomForestClassifier(n_estimators=20,criterion='entropy',min_samples_split=3)
 svm_clf = LinearSVC(C=1.0)
 mlp_clf = MLPClassifier(hidden_layer_sizes=(8,))
-brf_clf = BalancedRandomForestClassifier(n_estimators=20,criterion='entropy',min_samples_split=3)
 ##########################################
 
 ##########################################
@@ -88,10 +86,3 @@ res = pd.DataFrame(grid.cv_results_)
 res.to_csv('./Results/Resample_Results.csv',index=False)
 ##########################################
 
-##########################################
-# Balanced Random Forest
-grid = GridSearchCV(estimator=brf_clf,cv=spliter,refit=False,param_grid={},scoring=scorers,n_jobs=-2)
-grid.fit(df,target)
-res = pd.DataFrame(grid.cv_results_)
-res.to_csv('./Results/Balanced_RF_Results.csv',index=False)
-##########################################
